a3244b64e8f71e85640e142ab623205fb68b0b12
YeonHwan Park <yeonhwan.p@gmail.com>
2018-06-18 12:26:33 -0400

    Initialization

---
+++ RealUDPClient/RealUDPClient/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using ProtoBuf;
+
+namespace RealUDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool _done = false;
+            const int listenPort = 12000;
+            const int sendPort = 11000;
+
+            UdpClient listener = new UdpClient(listenPort);
+
+            while (!_done)
+            {
+                // Setting the sockets up
+                Socket thisSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPAddress target = IPAddress.Parse("127.0.0.1");
+
+                //byte[] Message = Encoding.ASCII.GetBytes("This is working!");
+
+                byte[] sendObject = Protobuf.SerializeData();
+                IPEndPoint theEndPoint = new IPEndPoint(target, sendPort);
+
+                Console.WriteLine("testing 1 {0}", sendObject.Length);
+
+                thisSocket.SendTo(sendObject, theEndPoint);
+
+                Console.WriteLine("Message sent to the broadcast address.");
+                Console.WriteLine("Would you like to wait for a response?");
+                string userResponse = Console.ReadLine();
+
+                if (userResponse.ToLower() == "no")
+                    _done = true;
+                else
+                {
+
+                    byte[] sendMessage = Encoding.ASCII.GetBytes("SENDBACK");
+                    MemoryStream tempStream = new MemoryStream();
+
+                    thisSocket.SendTo(sendMessage, theEndPoint);
+                    Console.WriteLine("Waiting...");
+
+                    byte[] receivedData = listener.Receive(ref theEndPoint);
+
+                    tempStream.Position = 0;
+                    tempStream.Write(receivedData, 0, receivedData.Length);
+
+                    Console.WriteLine("Here is the data:");
+
+                    Book receivedBook = Serializer.Deserialize<Book>(tempStream);
+
+                    Console.WriteLine(receivedBook.ToString());
+                    Console.WriteLine(Encoding.ASCII.GetString(receivedData, 0, receivedData.Length));
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+    class Protobuf
+    {
+
+        /// <summary>
+        /// Test function: Returns a byte array of a serialized object.
+        /// </summary>
+        public static byte[] SerializeData()
+        {
+            // Creating necessary tools to serialize
+            MemoryStream lStream = new MemoryStream();
+            BinaryWriter lWriter = new BinaryWriter(lStream);
+
+            // Test book object
+            Book lBook = methods.GetData();
+
+            // Serializing the object
+            Serializer.Serialize(lStream, lBook);
+            lWriter.Flush();
+            lStream.Position = 0;
+
+            byte[] returning = lStream.ToArray();
+
+            lWriter.Close();
+            lStream.Close();
+
+            return returning;
+        }
+    }
+}
---
+++ RealUDPServer/RealUDPServer/Server.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace RealUDPServer
+{
+    class Default
+    {
+        static void Main(string[] args)
+        {
+
+            UDPServer.StartListener();
+
+        }
+    }
+
+    public class UDPServer
+    {
+
+        private const int ListenPort = 11000;
+        private const int ReplyPort = 12000;
+
+        public static void StartListener()
+        {
+
+            bool done = false;
+
+            UdpClient listener = new UdpClient(ListenPort);
+            IPEndPoint other = new IPEndPoint(IPAddress.Any, ListenPort);
+
+            try
+            {
+                while (!done)
+                {
+                    Console.WriteLine("Receiving data...");
+                    byte[] initial = listener.Receive(ref other);
+
+                    Console.WriteLine("Received data from {0}. \n {1} \n",
+                                        other.ToString(),
+                                        Encoding.ASCII.GetString(initial, 0, initial.Length));
+                    Console.WriteLine("Waiting for sendback request...");
+
+                    byte[] toSendBack = listener.Receive(ref other);
+                    string theReply = Encoding.ASCII.GetString(toSendBack, 0, toSendBack.Length);
+
+                    if (theReply.ToUpper() == "SENDBACK")
+                    {
+                        Console.WriteLine("Request for sendback receieved, sending back...");
+
+                        Console.WriteLine("Debugging {0}", initial);
+
+                        SendBack(other.Address, initial, ReplyPort);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Sendback request denied.");
+                    }
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        public static void SendBack(IPAddress otherIP, byte[] data, int replyPort)
+        {
+            Socket thisSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint replyEndPoint = new IPEndPoint(otherIP, replyPort);
+
+            Console.WriteLine("testing again {0}", data.Length);
+
+            thisSocket.SendTo(data, replyEndPoint);
+            Console.WriteLine("Data sent back.");
+            Console.WriteLine();
+        }
+
+    }
+}
