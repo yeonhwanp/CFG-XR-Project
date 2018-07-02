5df34818e6406dba553cab279db8c48ee204a20a
YeonHwan Park <yeonhwan.p@gmail.com>
2018-07-02 18:25:40 -0400

    Structure of ConstructionManager completed

--- RobotScript/Assets/Scripts/ConstructionManager.cs
+++ RobotScript/Assets/Scripts/ConstructionManager.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//NOTE: Want to set objects as inactive as we spawn them because... it would look weird otherwise.
+
+/// <summary>
+/// Class to construct the data from the protobuf
+/// </summary>
 public class ConstructionManager : MonoBehaviour {
 
+    // Main overarching method to generate the entire robot itself
     public static void GenerateRobot(RobotStructure structure)
     {
-        JointStorage root = structure.rootJoint;
-        // Now need methods here to Look at the chidlren and make the robot from there...
-        Vector3 spawnPos = new Vector3(root.xLoc, root.yLoc, root.zLoc);
-        Quaternion spawnRot = new Quaternion(root.xRot, root.yRot, root.zRot, root.wRot);
-        GameObject rootObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        rootObject.transform.position = spawnPos;
-        rootObject.transform.rotation = spawnRot;
-        ObjectJoint rootJComponent = rootObject.AddComponent<ObjectJoint>();
+        Dictionary<int, GameObject> jointDict = new Dictionary<int, GameObject>();
+        Dictionary<int, GameObject> linkDict = new Dictionary<int, GameObject>();
+
+        // Actually, can we set up all of the things inside the dictionaries then just pair them up later? Sounds good?
+        foreach(KeyValuePair<int, JointStorage> joints in structure.JointDict)
+        {
+            GenerateJoint();
+        }
+
+        foreach(KeyValuePair<int, LinkStorage> links in structure.LinkDict)
+        {
+            GenerateLink();
+        }
+    }
+
+    // Method to make each joint
+    public static void GenerateJoint()
+    {
+
+    }
+
+    // Method to make each link
+    public static void GenerateLink()
+    {
+
     }
 }
