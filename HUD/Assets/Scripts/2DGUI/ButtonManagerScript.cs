16232eb8ff327335647aabe2bf97ae8a799f1023
YeonHwan Park <yeonhwan.p@gmail.com>
2018-07-18 14:22:23 -0400

    Started on scaling

--- HUD/Assets/Scripts/2DGUI/ButtonManagerScript.cs
+++ HUD/Assets/Scripts/2DGUI/ButtonManagerScript.cs
@@ -7,11 +7,42 @@ public class ButtonManagerScript : MonoBehaviour {
 
     public Button JointButton;
     public Button LinkButton;
+    public Button MoveButton;
+    public Button TransformButton;
+    public Button ScaleButton;
+
+    // Used to use specific modes of manipulation.
+    public EnabledButton enabledButton;
+
+    public enum EnabledButton
+    {
+        MoveButton,
+        TransformButton,
+        ScaleButton
+    };
 
     private void Start()
     {
         JointButton.onClick.AddListener(SpawnJoint);
         LinkButton.onClick.AddListener(SpawnLink);
+        MoveButton.onClick.AddListener(EnableCameraMovement);
+        TransformButton.onClick.AddListener(EnableTransform);
+        ScaleButton.onClick.AddListener(EnableScaling);
+    }
+
+    private void EnableTransform()
+    {
+        enabledButton = EnabledButton.TransformButton;
+    }
+
+    private void EnableCameraMovement()
+    {
+        enabledButton = EnabledButton.MoveButton;
+    }
+
+    private void EnableScaling()
+    {
+        enabledButton = EnabledButton.ScaleButton;
     }
 
     private void SpawnJoint()
--- HUD/Assets/Scripts/2DGUI/ClickerTest.cs
+++ HUD/Assets/Scripts/2DGUI/ClickerTest.cs
@@ -8,6 +8,12 @@ using UnityEngine;
 public class ClickerTest : MonoBehaviour {
 
     float distance = 3;
+    GameObject ButtonManager;
+
+    // TEST
+    float sizingFactor = 0.02f;
+    private float startSize;
+    private float startX;
 
     private void OnMouseDown()
     {
@@ -18,20 +24,69 @@ public class ClickerTest : MonoBehaviour {
     private void Start()
     {
         gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
+        ButtonManager = GameObject.Find("ButtonManager");
     }
 
     private void Update()
     {
+        // For the selection color
         if (GameObject.Find("Plane").GetComponent<SelectorManagerScript>().selected != gameObject)
         {
             gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
         }
+
+        //for resizing
+        if (ButtonManager.GetComponent<ButtonManagerScript>().enabledButton == ButtonManagerScript.EnabledButton.ScaleButton)
+        {
+            // Setting the values on buttondown
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+                startX = position.x;
+                position = Camera.main.ScreenToWorldPoint(position);
+                startSize = gameObject.transform.localScale.z;
+            }
+
+            // Doing the actual stuff
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 size = gameObject.transform.localScale;
+                size.x = startSize + (Input.mousePosition.x - startX) * sizingFactor;
+                gameObject.transform.localScale = size;
+            }
+        }
+
+
+        //if (ButtonManager.GetComponent<ButtonManagerScript>().enabledButton == ButtonManagerScript.EnabledButton.ScaleButton)
+        //{
+        //    float startSize;
+
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        Vector3 mouseOriginal = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        //        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseOriginal);
+        //        startSize = gameObject.transform.localScale.x;
+        //    }
+
+        //    if (Input.GetMouseButton(0))
+        //    {
+        //        Vector3 originalSize = gameObject.transform.localScale;
+        //        float newX = originalSize.x + (Input.mousePosition.x - startX) * sizingFactor;
+        //        Vector3 newSize = new Vector3(newX, originalSize.y, originalSize.z);
+        //        transform.localScale = newSize;
+        //    }
+
+        //}
     }
 
     private void OnMouseDrag()
     {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        gameObject.transform.position = objPosition;
+        // moving object
+        if (ButtonManager.GetComponent<ButtonManagerScript>().enabledButton == ButtonManagerScript.EnabledButton.TransformButton)
+        {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            gameObject.transform.position = objPosition;
+        }
     }
 }
