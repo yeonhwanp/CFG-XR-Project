c2348d0ebce509b6e2892c204fb23c7b81d4e83f
YeonHwan Park <yeonhwan.p@gmail.com>
2018-08-14 01:38:09 -0400

    Scaling working properly

---
+++ HUD/Assets/Scripts/ARGUI/XRinteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+using Leap.Unity.Query;
+using System;
+
+namespace Leap.Unity.Examples
+{
+    public class XRInteraction : MonoBehaviour
+    {
+
+        Controller controller;
+        List<Hand> HandList = new List<Hand>();
+
+        private void Start()
+        {
+            controller = new Controller();
+        }
+
+        private void FixedUpdate()
+        {
+            Frame frame = controller.Frame();
+            HandList = frame.Hands;
+            Vector3 original = transform.position;
+        }
+    }
+}
+
+
