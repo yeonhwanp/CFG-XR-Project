cdf2c1615aa1e58f77918942d728a7e0de5a4c93
YeonHwan <parky@mit.edu>
2019-06-14 00:22:06 -0400

    Added RobotLink doc

--- RobotMoverML/Assets/Scripts/ProtosAndConstruction/RobotLink.cs
+++ RobotMoverML/Assets/Scripts/ProtosAndConstruction/RobotLink.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Quite useless at the moment, but might have some use in the future.
-/// Stores all of the values.
+/// RobotLink: Stores all of the properties of a link.
+/// Not used at the moment.
+///
+/// Attributes:
+///     SelfObject: The GameObject that this RobotLink object is attached to.
+///     localCOM: The center of mass of this link.
+///     intertiaTensor: The inertiaTensor of this link.
+///     mass: The mass of this link.
 /// </summary>
 public class RobotLink : MonoBehaviour
 {
 
-    #region Public Variables
-
-    // General Properties of the GameObject
     public GameObject SelfObject;
     public Vector3 localCOM;
     public Vector3 inertiaTensor;
     public float mass;
 
-    #endregion
-
-    /// <summary>
-    /// Initializer
-    /// </summary>
     private void Start()
     {
         Rigidbody thisObjectRB = gameObject.GetComponent<Rigidbody>();
